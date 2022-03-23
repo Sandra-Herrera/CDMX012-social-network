146f1bcfdd2f825b614632d1be66760d772bf344
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-03-22 20:54:30 -0600

    Co-authored-by: Ana Cifuentes <AnaCi98@users.noreply.github.com> Co-authored-by: m-huesca <m-huesca@users.noreply.github.com>

--- src/components/register.js
+++ src/components/register.js
@@ -1,23 +1,68 @@
 // eslint-disable-next-line import/no-cycle
 import { createUserRed } from '../lib/firebase.js';
+// eslint-disable-next-line import/no-cycle
+import { onNavigate } from '../main.js';
 
-export const register = `
-<a onclick="onNavigate('/'); return
-      false"><img class="arrowBack" src="img/Arrow.png"></img></a>
-<div class="containerRegister">
-    <img class="logo" src="img/logoSize.png">
-  <section class="registerText">
-    <p> Registrate con tu correo <p>
-  </section>
-  <div class="containerInputR">
-    <input id="mail" type="text" class="register" placeholder="Correo electrónico" autocomplete="off"> 
-    <input id="user" type="text" class="register" placeholder="Nombre de usuaria" autocomplete="off"> 
-    <button id = "maskify" class = "maskify" onclick = "maskifyPass()"> HERE</button>
-    <input id="password" type="password" class="register" placeholder="Contraseña" autocomplete="off"> 
-    <input id="area" type="text" class="register" placeholder="Area tech" autocomplete="off"> 
-    <button id="registerButton" class="registerButton" onclick="registerFireBase(event)">Registrarme</button>
-  </div>
-</div>`;
+export const register = () => {
+  const containerAllRegister = document.createElement('section');
+  const arrowBack = document.createElement('img');
+  arrowBack.setAttribute('src', 'img/Arrow.png');
+  arrowBack.setAttribute('class', 'arrowBack');
+  const containerRegister = document.createElement('section');
+  containerRegister.setAttribute('class', 'containerRegister');
+  const logo = document.createElement('img');
+  logo.setAttribute('class', 'logo');
+  logo.setAttribute('src', 'img/logoSize.png');
+  const registerText = document.createElement('section');
+  registerText.setAttribute('class', 'registerText');
+  const paragraph = document.createElement('p');
+  paragraph.textContent = 'Registrate con tu correo';
+  registerText.appendChild(paragraph);
+  const containerInputR = document.createElement('section');
+  containerInputR.setAttribute('class', 'containerInputR');
+  const mail = document.createElement('input');
+  mail.setAttribute('class', 'register');
+  mail.setAttribute('id', 'mail');
+  mail.setAttribute('type', 'text');
+  mail.setAttribute('placeholder', 'Ingresa tu correo');
+  mail.setAttribute('autocomplete', 'off');
+  const user = document.createElement('input');
+  user.setAttribute('class', 'register');
+  user.setAttribute('id', 'user');
+  user.setAttribute('type', 'text');
+  user.setAttribute('placeholder', 'Ingresa tu nombre de usuaria');
+  user.setAttribute('autocomplete', 'off');
+  const password = document.createElement('input');
+  password.setAttribute('class', 'register');
+  password.setAttribute('id', 'password');
+  password.setAttribute('type', 'text');
+  password.setAttribute('placeholder', 'Ingresa tu contraseña');
+  password.setAttribute('autocomplete', 'off');
+  const area = document.createElement('input');
+  area.setAttribute('class', 'register');
+  area.setAttribute('id', 'area');
+  area.setAttribute('type', 'text');
+  area.setAttribute('placeholder', 'Ingresa tu area tech');
+  area.setAttribute('autocomplete', 'off');
+  const registerButton = document.createElement('button');
+  registerButton.setAttribute('class', 'registerButton');
+  registerButton.setAttribute('id', 'registerButton');
+  registerButton.textContent = 'Registrarme';
+  containerInputR.appendChild(mail);
+  containerInputR.appendChild(user);
+  containerInputR.appendChild(password);
+  containerInputR.appendChild(area);
+  containerInputR.appendChild(registerButton);
+  containerRegister.appendChild(logo);
+  containerRegister.appendChild(registerText);
+  containerRegister.appendChild(containerInputR);
+  containerAllRegister.appendChild(arrowBack);
+  containerAllRegister.appendChild(containerRegister);
+  arrowBack.addEventListener('click', () => {
+    onNavigate('/');
+  });
+  return containerAllRegister;
+};
 
 function registerFireBase(e) {
   e.preventDefault();
