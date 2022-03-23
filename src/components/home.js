c8b6a533b863baf8fa64346825d21ad4870ec432
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-03-23 15:32:53 -0600

    cambios home

--- src/components/home.js
+++ src/components/home.js
@@ -40,18 +40,3 @@ export const home = () => {
 
   return sectionStart;
 };
-//`
-// <div class="containerStart">
-// <img class="logo" src="img/logoSize.png">
-// <section class="welcomeText">
-// <p> ¡Bienvenida! <p>
-// <p>Conecta con tu mundo tech. <p>
-// </section>
-// <div class="containerButtons">
-// <a onclick="onNavigate('/register'); return
-// false;" class="button">Registrate</a>
-// <a onclick="onNavigate('/login'); return
-// false;" class="button">Iniciar sesión</a>
-// </div>
-// </div>
-// `;
